f201a71cdbcc0d32030f3993508befe38809ea81
pkd2210 <45825608+pkd2210@users.noreply.github.com>
2026-03-18 22:57:50 +0200

    Update description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,7 @@
     <!-- edit Those to fit your project -->
     <!--     <svg class="hero-svg" style="fill: #3c4858;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><circle cx="50%" cy="50%" r="240" fill="{config['theme-color']}" /><g transform="translate(320, 320) scale(0.6) translate(-320, -320)"><path d="M512 128C514 128 515.9 128.1 517.8 128.3L422.1 224L490 224L562 152C570.8 163 576 176.9 576 192L576 448C576 483.3 547.3 512 512 512L128 512C92.7 512 64 483.3 64 448L64 192C64 156.7 92.7 128 128 128L198.1 128L102.1 224L170 224L265 129L266 128L358.1 128L262.1 224L330 224L425 129L426 128L512.1 128z"/></g></svg>-->
     <div class="title">Welcome to {config["ysws-name"]}'s Shop</div>
-    <p class="subtitle" style="text-align: center">You Ship: Something that connect to an existing application (n8n node, minecraft mode, Discord / slack bot, more....)<br>We Ship: Everything the comes with a cable (Cables, mouses, keyboards, more...)</p>
+    <p class="subtitle" style="text-align: center">You Ship: Something that connect to an existing application (n8n node, minecraft mod, Discord / slack bot, more....)<br>We Ship: Everything the comes with a cable (Cables, mouses, keyboards, more...)</p>
     <!--End of the editing section-->
     <button class="slack-button"><a href="https://hackclub.enterprise.slack.com/archives/C0ALR4T650U" target="_blank">#cable-ysws</a></button>
     <style>
